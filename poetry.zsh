ae25f3b0e301fb776600b74140a73d6e8f6978e8
Marc Moody <marc.moody@xyngular.com>
2019-07-08 13:52:51 -0600

    Update command for poetry >= 1

--- poetry.zsh
+++ poetry.zsh
@@ -14,7 +14,7 @@ _zp_check_poetry_venv() {
   fi
   if [[ -f pyproject.toml ]] \
       && [[ "${PWD}" != "${_zp_current_project}" ]]; then
-    venv="$(command poetry debug:info 2>/dev/null | grep -oP '.*Path:\s+\K.*')"
+    venv="$(command poetry env info --path 2>/dev/null | grep -oP '.*Path:\s+\K.*')"
     if [[ -d "$venv" ]] && [[ "$venv" != "$VIRTUAL_ENV" ]]; then
       source "$venv"/bin/activate || return $?
       _zp_current_project="${PWD}"
